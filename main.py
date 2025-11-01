6c17b7381d9832d613b40eb91d7c9e4950f7c00b
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-11-01 11:50:58 +0300

    Improving the UI & UX

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from tkinter import *
 
 FONT_NAME = "Courier"
+entry_style = {
+    "highlightthickness": 1,      
+    "highlightbackground": "green", 
+    "highlightcolor": "red",      
+    "bd": 0,                       
+    "font": (FONT_NAME, 10, "bold")
+}
+
 
 window = Tk()
 window.title("Password Manager")
@@ -16,20 +24,22 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:", font=(FONT_NAME, 10, "bold"))
 website_label.grid(column=0, row=1)
 
-website_entry = Entry()
-website_entry.grid(column=1, row=1, sticky="ew", columnspan=2)
+website_entry = Entry(**entry_style)
+website_entry.grid(column=1, row=1, sticky="ew", columnspan=2, pady=3)
+website_entry.focus()
 
 email_label = Label(text="Email/Username:", font=(FONT_NAME, 10, "bold"))
 email_label.grid(column=0, row=2)
 
-email_entry = Entry()
-email_entry.grid(column=1, row=2, sticky="ew", columnspan=2)
+email_entry = Entry(**entry_style)
+email_entry.grid(column=1, row=2, sticky="ew", columnspan=2, pady=3)
+email_entry.insert(0, "khaled.elsayed2206@gmail.com")
 
 password_label = Label(text="Password:", font=(FONT_NAME, 10, "bold"))
 password_label.grid(column=0, row=3)
 
-password_entry = Entry()
-password_entry.grid(column=1, row=3, sticky="we")
+password_entry = Entry(**entry_style)
+password_entry.grid(column=1, row=3, sticky="we", pady=3)
 
 password_gen_button = Button(text="Generate Password", fg="#0000CD", bg="#F0FFFF", font=(FONT_NAME, 10, "bold"))
 password_gen_button.grid(column=2, row=3, padx=5)
